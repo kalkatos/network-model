2ca9c8476b3570d01b0817a301e2cbd9aa508c6d
Alex Kalkatos <alexcsts@gmail.com>
2022-12-28 00:59:48 -0300

    Player Registry

---
+++ PlayerRegistry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FunctionsGame.NetworkModel
+{
+	public class PlayerRegistry
+	{
+		public string PlayerId;
+		public bool IsAuthenticated;
+		public string[] Devices;
+		public DateTime LastAccess;
+		public DateTime FirstAccess;
+		public DateTime TimeOfAuthentication;
+	}
+}
