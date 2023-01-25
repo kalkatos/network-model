9c10989de9210527308c087744898eabd8de8782
Alex Kalkatos <alexcsts@gmail.com>
2023-01-25 08:56:15 -0300

    Match response

---
+++ Responses/MatchResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalkatos.Network.Model
+{
+	public class MatchResponse
+	{
+		public string MatchId;
+		public string[] Players;
+		public bool IsError;
+		public string ErrorMessage;
+	}
+}
