a53875d7e0a33791d9c3fea33d0b8d7bd78a2f38
Alex Kalkatos <alexcsts@gmail.com>
2023-01-29 00:41:26 -0300

    Clone player info

--- Info/PlayerInfo.cs
+++ Info/PlayerInfo.cs
@@ -5,5 +5,22 @@
 		public string Alias;
 		public string Nickname;
 		public string[] CustomData;
+
+		public PlayerInfo Clone ()
+		{
+			string[] newCustomData = null;
+			if (CustomData != null)
+			{
+				newCustomData = new string[CustomData.Length];
+				for (int i = 0; i < CustomData.Length; i++)
+					newCustomData[i] = CustomData[i];
+			}
+			return new PlayerInfo
+			{
+				Alias = Alias,
+				Nickname = Nickname,
+				CustomData = newCustomData
+			};
+		}
 	}
 }
