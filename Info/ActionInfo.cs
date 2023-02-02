e2c201dfb2148e3b1e39715574831cb0ed016f14
Alex Kalkatos <alexcsts@gmail.com>
2023-02-02 10:23:05 -0300

    Player alias for actions

--- Info/ActionInfo.cs
+++ Info/ActionInfo.cs
@@ -2,6 +2,7 @@
 {
 	public class ActionInfo
 	{
+		public string PlayerAlias;
 		public string ActionName;
 		public object Parameter;
 	}
--- Requests/ActionRequest.cs
+++ Requests/ActionRequest.cs
@@ -3,6 +3,7 @@
 	public class ActionRequest
 	{
 		public string PlayerId;
+		public string PlayerAlias;
 		public string MatchId;
 		public string ActionName;
 		public string SerializedParameter;
