55286f1962768ac7a5bdf03ce9ceefb0d4653422
Alex Kalkatos <alexcsts@gmail.com>
2023-02-05 00:52:41 -0300

    Action as a state changer WIP

--- Info/ActionInfo.cs
+++ Info/ActionInfo.cs
@@ -1,8 +1,11 @@
-﻿namespace Kalkatos.Network.Model
+﻿using System.Collections.Generic;
+
+namespace Kalkatos.Network.Model
 {
 	public class ActionInfo
 	{
 		public string PlayerAlias;
+		public Dictionary<string, string> Content;
 		public string ActionName;
 		public object Parameter;
 	}
--- Requests/ActionRequest.cs
+++ Requests/ActionRequest.cs
@@ -1,10 +1,13 @@
-﻿namespace Kalkatos.Network.Model
+﻿using System.Collections.Generic;
+
+namespace Kalkatos.Network.Model
 {
 	public class ActionRequest
 	{
 		public string PlayerId;
 		public string PlayerAlias;
 		public string MatchId;
+		public Dictionary<string, string> Content;
 		public string ActionName;
 		public string SerializedParameter;
 	}
