3515abecb70c4fa0ff3386ed62dfa9b2872c4227
Alex Kalkatos <alexcsts@gmail.com>
2023-02-09 17:06:36 -0300

    StateInfo QoL methods

--- Info/StateInfo.cs
+++ Info/StateInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Kalkatos.Network.Model
@@ -18,6 +19,46 @@ namespace Kalkatos.Network.Model
 			clone.PrivateProperties = PrivateProperties.ToDictionary(e => e.Key, e => e.Value);
 			clone.Hash = Hash;
 			return clone;
+		}
+
+		public bool HasAnyPublicProperty ()
+		{
+			return PublicProperties != null && PublicProperties.Count > 0;
+		}
+
+		public bool HasAnyPrivateProperty ()
+		{
+			return PrivateProperties != null && PrivateProperties.Count > 0;
+		}
+
+		public bool HasPublicProperty (string key) 
+		{
+			return PublicProperties != null && PublicProperties.ContainsKey(key);
+		}
+
+		public bool IsPublicPropertyEqualsIfPresent (string key, params string[] values)
+		{
+			return !HasPublicProperty(key) || (values != null && values.Contains(PublicProperties[key]));
+		}
+
+		public bool HasPrivateProperty (string key)
+		{
+			return PrivateProperties != null && PrivateProperties.ContainsKey(key);
+		}
+
+		public bool IsPrivatePropertyEqualsIfPresent (string key, params string[] values)
+		{
+			return !HasPublicProperty(key) || (values != null && values.Contains(PrivateProperties[key]));
+		}
+
+		public bool OnlyHasThesePublicProperties (params string[] keys)
+		{
+			return PublicProperties == null || PublicProperties.Keys.All(key => keys.Contains(key));
+		}
+
+		public bool OnlyHasThesePrivateProperties (params string[] keys)
+		{
+			return PrivateProperties == null || PrivateProperties.Keys.All(key => keys.Contains(key));
 		}
 	}
 }
