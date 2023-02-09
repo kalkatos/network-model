edd8651ded5a31a9adb2cc5cd1cdcd3aa16da6f0
Alex Kalkatos <alexcsts@gmail.com>
2023-02-09 04:03:02 -0300

    Clone for state info

--- Info/StateInfo.cs
+++ Info/StateInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Kalkatos.Network.Model
 {
@@ -8,5 +9,15 @@ namespace Kalkatos.Network.Model
 		public Dictionary<string, string> PublicProperties;
 		public Dictionary<string, string> PrivateProperties;
 		public int Hash;
+
+		public StateInfo Clone ()
+		{
+			StateInfo clone = new StateInfo ();
+			clone.Index = Index;
+			clone.PublicProperties = PublicProperties.ToDictionary(e => e.Key, e => e.Value);
+			clone.PrivateProperties = PrivateProperties.ToDictionary(e => e.Key, e => e.Value);
+			clone.Hash = Hash;
+			return clone;
+		}
 	}
 }
