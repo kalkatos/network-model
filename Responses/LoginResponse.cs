127b936261fedc576abfc8f3303ba6adb65cc49f
Alex Kalkatos <alexcsts@gmail.com>
2023-03-17 16:07:05 -0300

    Must run locally setting in login response

--- Responses/LoginResponse.cs
+++ Responses/LoginResponse.cs
@@ -3,6 +3,7 @@
 	public class LoginResponse : Response
 	{
         public bool IsAuthenticated;
+        public bool MustRunLocally;
         public string PlayerId;
         public PlayerInfo MyInfo;
     }
