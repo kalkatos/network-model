99a1cecb9a5f71c684aa7b0e2a5a2c9189aceff2
Alex Kalkatos <alexcsts@gmail.com>
2025-01-06 23:28:42 -0300

    User info clone

--- Info/UserInfo.cs
+++ Info/UserInfo.cs
@@ -6,5 +6,15 @@
 		public string Email;
 		public string Picture;
 		public string UserId;
+
+		public UserInfo () { }
+
+		public UserInfo (UserInfo other)
+		{
+			Name = other.Name;
+			Email = other.Email;
+			Picture = other.Picture;
+			UserId = other.UserId;
+		}
 	}
 }
